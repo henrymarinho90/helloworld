1d59fb744ad8a4d76d1577ad5baf0be9223797dc
Henry Alexander Mariño Avila <henrymarinho90@gmail.com>
2022-11-03 22:06:21 -0500

    Pruebita

---
+++ 1stfile.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  3 16:09:18 2022
+
+@author: Henry
+"""
+
+Edad=32;
+nombre="Henry";
+alto=True;
+estatura=1.83;
+print("Edad ",Edad, " nombre", nombre,"Es alto?", alto, "Estatura ", estatura);
+
+
+
